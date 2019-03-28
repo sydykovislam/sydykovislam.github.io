07b32a02c86dd18a1aa2dbb34b9cfd297ea7a22f
Sydykov Islam <iskasydykov@gmail.com>
2019-03-29 00:05:20 +0600

    Add a lot

--- scripts.js
+++ scripts.js
@@ -1,26 +1,35 @@
 let contactsModalBtn = document.querySelector(".contacts-div");
+let contactsModalBtn2 = document.querySelector("#contacts-nav-btn");
 let contactsModal = document.querySelector(".contacts-modal");
 let contBackdrop = document.querySelector(".backdrop");
 
 function openModal() {
-  let scrollHeight = Math.max(
-    document.body.scrollHeight,
-    document.documentElement.scrollHeight,
-    document.body.offsetHeight,
-    document.documentElement.offsetHeight,
-    document.body.clientHeight,
-    document.documentElement.clientHeight
-  );
-
-  // document.contactsModal.style.top = scrollHeight + "px";
-
   contactsModal.className = "contacts-modal modal-on";
   contBackdrop.className = "backdrop backdrop-on";
 }
 function closeModal() {
   contactsModal.className = "contacts-modal modal-off";
   contBackdrop.className = "backdrop backdrop-off";
+
+  buttonText.innerHTML = "Send";
+  sendBtn.classList.toggle("button__circle");
 }
 
 contactsModalBtn.addEventListener("click", openModal);
+contactsModalBtn2.addEventListener("click", openModal);
 contBackdrop.addEventListener("click", closeModal);
+
+let sendBtn = document.querySelector(".button");
+let buttonText = document.querySelector(".tick");
+
+const tickMark =
+  '<svg width="58" height="45" viewBox="0 0 58 45" xmlns="http://www.w3.org/2000/svg"><path fill="#fff" fill-rule="nonzero" d="M19.11 44.64L.27 25.81l5.66-5.66 13.18 13.18L52.07.38l5.65 5.65"/></svg>';
+
+buttonText.innerHTML = "Send";
+
+sendBtn.addEventListener("click", function() {
+  if (buttonText.innerHTML === "Send") {
+    buttonText.innerHTML = tickMark;
+    this.classList.toggle("button__circle");
+  }
+});
